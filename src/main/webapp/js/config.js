3b1f6f77e490fe0eb341b88c211dd624bb95f1bb
mbucknel <mbucknel@usgs.gov>
2012-01-24 20:06:16 +0000

    Added an additional base layer of SDDS imagery.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/glri-phragmites-map/trunk@20114 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -9,9 +9,22 @@ GLRI.ui.map.XYZ_URL_POSTFIX = '${z}/${y}/${x}';
 GLRI.ui.map.baseLayers = [
       { 
       	name: "ESRI Shaded Relief World 2D", 
-          url: "http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_ShadedRelief_World_2D/MapServer/tile/"+GLRI.ui.map.XYZ_URL_POSTFIX,
-          type: OpenLayers.Layer.XYZ
-      }
+        url: "http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_ShadedRelief_World_2D/MapServer/tile/"+GLRI.ui.map.XYZ_URL_POSTFIX,
+        type: OpenLayers.Layer.XYZ
+      },
+      {
+    	name: "SDDS Imagery",
+    	url: "http://isse.cr.usgs.gov/ArcGIS/services/Combined/SDDS_Imagery/MapServer/WMSServer",
+    	type: OpenLayers.Layer.WMS,
+    	layers: '0'
+// Removed the wetlands map for now as it doesn't appear to have map data.
+//      },
+//      {
+//      	name: "FWS Wetlands",
+//      	url: "http://137.227.242.85/arcGIS/services/FWS_Wetlands_WMS/MapServer/WMSServer",
+//      	type: OpenLayers.Layer.WMS,
+//      	layers: '17'
+        }     
   ];
 
 GLRI.ui.map.habitatLayers = [
