c80fb382b59d9dc26ac1a1c3b06f5fc9955f4dd2
mbucknel <mbucknel@usgs.gov>
2012-02-21 17:44:01 +0000

    Added two new functions. GLRI.ui.getLegendHTML is used to return the <img> element for the legend at url using layer identifier layer.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/glri-phragmites-map/trunk@20819 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -30,25 +30,40 @@ GLRI.ui.initMap = function() {
 	};
 	GLRI.ui.map.mainMap.zoomTo(5);
 	GLRI.ui.map.mainMap.panTo(new OpenLayers.LonLat(-84, 45));
-}
+};
 GLRI.ui.turnOnHabitatLayerMap = function(name, layers){
 	for(var i = 0; i < layers.length; i++){
 		if(layers[i].name==name){
-			GLRI.ui.map.mainMap.addLayer(new layers[i].type(
-				layers[i].name,
-				layers[i].url,
-	            {
-					layers: layers[i].layers,
-					transparent: true},
-				{	
-					displayInLayerSwitcher: false,
-					singleTile: true
-				}
-	        ));
+			var layer_to_add = new layers[i].type(
+					layers[i].name,
+					layers[i].url,
+		            {
+						layers: layers[i].layers,
+						transparent: true},
+					{	
+						displayInLayerSwitcher: false,
+						singleTile: true
+					});
+			GLRI.ui.map.mainMap.addLayer(layer_to_add);
+
 		} else {
 			var map = GLRI.ui.map.mainMap;
 			if(map.getLayersByName(layers[i].name)[0])
 				map.removeLayer(map.getLayersByName(layers[i].name)[0], false);
 		}
 	}
-}
+};
+GLRI.ui.getLegendHTML = function(url, layer){
+	// Return the request string for the map legend at url and the layers identifier.
+	return '<img src=' + url + '?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=' + layer + '>';
+};
+GLRI.ui.turnOnLegend = function(name, layers, div_id){
+	// Show the legend information for the layer in layers with a name equal to name in the div element, div_id.
+	for(var i = 0; i < layers.length; i++){
+		if (layers[i].name == name) {
+			document.getElementById(div_id).innerHTML = GLRI.ui.getLegendHTML(layers[i].url, layers[i].layers);
+			return;
+		}
+	}
+	document.getElementById(div_id).innerHTML='';
+};
